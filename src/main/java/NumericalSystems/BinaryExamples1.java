3fe464dcc4e0cc369f34850ceb8b63d5afd160ff
lia-Ak <129335873+lia-Ak@users.noreply.github.com>
2023-05-16 23:47:14 +0200

    java

---
+++ src/main/java/NumericalSystems/BinaryExamples1.java
@@ -0,0 +1,13 @@
+package NumericalSystems;
+
+public class homework {
+
+
+    public static void main(String[] args) {
+        int i = 232;
+        System.out.println( Integer.toBinaryString(i));
+        System.out.println(Integer.toOctalString(i));
+        System.out.println(Integer.toHexString(i));
+        System.out.println(Integer.parseInt(i));
+    }
+}

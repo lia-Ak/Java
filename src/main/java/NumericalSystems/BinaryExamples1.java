dae9702bd73eefb8c96a8ba5f6d7b17bebfe65c0
lia-Ak <129335873+lia-Ak@users.noreply.github.com>
2023-05-16 23:56:27 +0200

    homework

--- src/main/java/NumericalSystems/BinaryExamples1.java
+++ src/main/java/NumericalSystems/BinaryExamples1.java
@@ -1,13 +1,24 @@
 package NumericalSystems;
 
-public class homework {
+public class BinaryExamples1 {
 
 
     public static void main(String[] args) {
         int i = 232;
-        System.out.println( Integer.toBinaryString(i));
-        System.out.println(Integer.toOctalString(i));
+        System.out.println(Integer.toBinaryString(i));
+
+
         System.out.println(Integer.toHexString(i));
-        System.out.println(Integer.parseInt(i));
+
+
+        System.out.println(Integer.toOctalString(i));
+
+
+
+
+        System.out.println(Integer.valueOf(i));
+
+
+
     }
 }
---
+++ src/main/java/hom/homework.java
@@ -0,0 +1,26 @@
+package hom;
+
+public class homework{
+
+
+
+        public static void main(String[] args) {
+            int i = 232;
+            System.out.println(Integer.toBinaryString(i));
+
+
+            System.out.println(Integer.toHexString(i));
+
+
+            System.out.println(Integer.toOctalString(i));
+
+
+
+
+            System.out.println(Integer.valueOf(i));
+
+
+
+        }
+    }
+
